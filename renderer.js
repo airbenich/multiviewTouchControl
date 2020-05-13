9787a9227a8c2a3402620cf700926b2d58635a21
airbenich <vivian@erbenich.eu>
2020-05-13 19:50:19 +0200

    adds "connection ready" animation / feedback to user

--- renderer.js
+++ renderer.js
@@ -105,53 +105,22 @@ if(switcherip !== null) {
     grid.elementList['input-8'].onDoubleClick(function () {
         mainswitcher.cut();
     });
-}
 
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;                                                        
-var yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    console.log(xDiff);
     
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
-        } else {
-            /* right swipe */
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
-        } else { 
-            /* down swipe */
-        }                                                                 
+    // Software is up and running Feedback to User:
+    sendUpAndRunningFeedbackToUser();
+    
+    function sendUpAndRunningFeedbackToUser() {
+        var startAnimationSpeed = 100;
+        var startDelay = 1000;
+        var inputAnimationSelection = [1,2,3,4,8,7,6,5,5,4,3,2,1];
+        for (let index = 0; index < inputAnimationSelection.length; index++) {
+            setTimeout(function () {
+                mainswitcher.changePreviewInput(index);
+                // console.log('Switch ' + inputAnimationSelection[index]);
+                
+            },startDelay+index*startAnimationSpeed);
+        }
     }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+    
+}
